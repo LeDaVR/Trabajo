c62a4253661a2f411985e17d3d59eb3d6246e659
LeDaVR <danielvaldiviaramos@hotmail.com>
2018-10-24 19:07:43 -0500

    0.2

--- planting time.cpp
+++ planting time.cpp
@@ -2,101 +2,91 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
 using namespace std;
-class texturec{
+class AutoSprite{
 	public:
 		Texture texture;
 		string img;
-		int ancho,largo;
+		int ancho,largo,x,y;
 		Sprite sprite;
-		texturec(string _img,int _x,int _y){
+		AutoSprite(string _img){
 			texture.loadFromFile(_img);
-			ancho=_x;
-			largo=_y;
 			sprite.setTexture(texture);
-}
+		}	
+		void posdimensiones(int _ancho,int _largo,int _x,int _y){
+			ancho=_ancho;
+			largo=_largo;
+			x=_x;
+			y=_y;
+			sprite.setPosition(x,y);
+		}
+		void posajustar(int _x,int _y){
+			x=_x;y=_y;
+			sprite.setPosition(x,y);
+		}
 };
-void mostrarmenu(RenderWindow &a,texturec *arr,int len){
+void mostrarset(RenderWindow &a,AutoSprite *arr,int len){
 	for(int i=0;i<len;i++){
 		a.draw(arr[i].sprite);
 	}
 }
+void movermenu(AutoSprite &autosprite,int cx,int cy,int limitsup,int limitinf){
+	int x=autosprite.x;
+	int y=autosprite.y;
+	if (Keyboard::isKeyPressed(Keyboard::Up)&&y!=limitsup){
+		y-=cy;	
+		autosprite.posajustar(x,y);		
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Down)&&y!=limitinf){
+		y+=cy;	
+		autosprite.posajustar(x,y);
+	}
+}
 int main()
 {
-    // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    // Load a sprite to display
-	texturec jugar("img/MENUJUGAR.jpg",350,100);
-	jugar.sprite.setPosition(jugar.ancho,jugar.largo);
-	texturec creditos("img/MENUCREDITOS.jpg",350,250);
-	creditos.sprite.setPosition(creditos.ancho,creditos.largo);
-	texturec salir("img/MENUSALIR.jpg",350,400);
-	salir.sprite.setPosition(salir.ancho,salir.largo);
-	texturec select("img/SELECCIONAR.png",jugar.ancho,jugar.largo);
-	select.sprite.setPosition(select.ancho,select.largo);
-	texturec fondo("img/fondo.jpg",800,600);
-	texturec arr[]={fondo,jugar,creditos,salir,select};
-	texturec creditos1("img/fondo.jpg",400,200);
-	int mostrar=0;
-	texturec arr2[]={creditos1};
-    // Create a graphical text to display
-    /*sf::Font font;
-    if (!font.loadFromFile("arial.ttf"))
-        return EXIT_FAILURE;
-    sf::Text text("Hello SFML", font, 50);
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile("nice_music.ogg"))
-        return EXIT_FAILURE;
-    // Play the music
-    music.play();*/
-    // Start the game loop
+  	AutoSprite fondo("img/fondo.jpg");  
+	AutoSprite jugar("img/MENUJUGAR.jpg");
+	AutoSprite creditos("img/MENUCREDITOS.jpg");
+	AutoSprite salir("img/MENUSALIR.jpg");	
+	AutoSprite select("img/SELECCIONAR.png");		
+	jugar.posajustar(350,100);
+	creditos.posajustar(350,250);
+	salir.posajustar(350,400);
+	select.posajustar(350,100);
+	AutoSprite menuprincipal[]={fondo,jugar,creditos,salir,select};
+	AutoSprite menucreditos[]={fondo,salir,select};
+	menucreditos[2].posajustar(350,399);
+	int mostrar=0;	
     while (window.isOpen())
     {
- 		sf::Event event;
+   		Event event; 	
+		switch(mostrar){
+			case(0):{
+				window.clear();
+				mostrarset(window,menuprincipal,5);
+				window.display();
+				break;
+			}
+			case(1):{
+				window.clear();
+				mostrarset(window,menucreditos,3);
+				window.display();
+				break;
+			}
+		}
         while (window.pollEvent(event))
         {
-
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-					if(select.largo==250){
-            			arr[4].sprite.setPosition(350,100);
-            			select.largo=100;
-            			break;
-					}else if(select.largo==400){
-            			arr[4].sprite.setPosition(350,250);
-            			select.largo=250;
-					}		
+			if(mostrar==0){
+				movermenu(menuprincipal[4],0,150,100,400);
+				if (Keyboard::isKeyPressed(Keyboard::Return)&&menuprincipal[4].y==400)
+					window.close();
+				if (Keyboard::isKeyPressed(Keyboard::Return)&&menuprincipal[4].y==250){
+					mostrar=1;
+				}
 			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-					if(select.largo==250){
-            			arr[4].sprite.setPosition(350,400);
-            			select.largo=400;
-            			break;
-					}else if(select.largo==100){
-            			arr[4].sprite.setPosition(350,250);
-            			select.largo=250;
-					}		
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)&&select.largo==400)
-			    window.close();
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)&&select.largo==250){
-				mostrar=1;
-			}
-			   
         }
-        
-		if(mostrar==0){
-			window.clear();
-			mostrarmenu(window,arr,5);
-			window.display();
-	        // Process events
-		}else if(mostrar==1){
-			window.clear();
-			mostrarmenu(window,arr2,1);
-			window.display();
-		}
-		
     }
     return EXIT_SUCCESS;
 }
